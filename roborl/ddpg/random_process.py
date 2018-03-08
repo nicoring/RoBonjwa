f073b4e59d4be0317140548a32b54130c7c38550
Julian Habekost <julian.habekost@googlemail.com>
2018-03-08 01:42:12 +0000

    Set Ornstein-Uhlenbeck dt=0.01

--- roborl/ddpg/random_process.py
+++ roborl/ddpg/random_process.py
@@ -2,16 +2,17 @@ import numpy as np
 
 
 class OrnsteinUhlenbeckProcess:
-    def __init__(self, shape, theta=0.15, sigma=0.2):
+    def __init__(self, shape, theta=0.15, sigma=0.2, dt=1e-2):
         self.theta = theta
         self.mu = 0
         self.sigma = sigma
+        self.dt = dt
         self.last = 0
         self.shape = np.atleast_1d(shape)
 
     def sample(self):
-        self.last = self.last + self.theta * (self.mu - self.last) + \
-            self.sigma * np.random.randn(*self.shape)
+        self.last = self.last + self.theta * (self.mu - self.last) * self.dt + \
+            self.sigma * np.sqrt(self.dt) * np.random.randn(*self.shape)
         return self.last
 
     def reset(self, sigma=None):
