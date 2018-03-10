754fa7471ce0ae170f61a5fde357c6f0f0e71727
Chau Luu <s1786813@sms.ed.ac.uk>
2018-03-10 12:52:41 +0000

    Made ParamNoise class, committing before making changes to ddpg to make it take ExplorationClass

--- roborl/ddpg/noise.py
+++ roborl/ddpg/noise.py
@@ -1,1 +1,40 @@
+import numpy as np
+import torch
+
+class ParamNoise:
+
+    def __init__(self, batch_size, memory, sigma=0.1, delta=0.1, alpha=1.01):
+        self.alpha = alpha
+        self.batch_size = batch_size
+        self.memory = memory
+        self.sigma = sigma
+        self.delta = delta
+        self.orig_sigma = sigma
+
+    def distance(self, model, perturbed_model):
+        ''' DDPG Distance '''
+        batch = self.memory.sample_batch(self.batch_size)
+        return torch.sqrt(torch.mean((model(batch.state) - perturbed_model(batch.state))**2))
+
+    def update_sigma(self, model, perturbed_model):
+        '''Updates sigma at each timestep'''
+        dist = self.distance(model, perturbed_model)
+        if dist <= self.delta:
+            self.sigma *= self.alpha
+        else:
+            self.sigma *= 1 / self.alpha
+    
+    def perturb_model(self, model):
+        perturbed_model = model.clone()
+        for param in perturbed_model.parameters():
+            noise = torch.normal(means=torch.zeros_like(param), std=self.sigma)
+            param.add_(noise)
+        return perturbed_model
+
+    def reset(self):
+        self.sigma = self.orig_sigma
+        
+
+
+
 
