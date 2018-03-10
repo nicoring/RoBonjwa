6b72f136ca60cb801ad55a8b6994c2405f75f2af
Chau Luu <s1786813@sms.ed.ac.uk>
2018-03-10 13:40:35 +0000

    Moved away select_action into a method of an ExplorationPolicy, unifying behaviour for ddpg regardless of noise type

---
+++ roborl/ddpg/exploration.py
@@ -0,0 +1,56 @@
+import numpy as np
+import torch
+
+from random_process import OrnsteinUhlenbeckProcess
+from noise import ParamNoise
+
+class ActionNoisePolicy:
+
+    def __init__(self, actor, env, ou_theta, ou_sigma):
+        self.actor = actor
+        self.random_process = OrnsteinUhlenbeckProcess(env.action_space.shape[0],
+                                                       theta=ou_theta, sigma=ou_sigma)
+    
+    def select_action(self, state, exploration=True):
+        if use_cuda:
+            state = state.cuda()
+        self.actor.eval()
+        action = self.actor(state)
+        self.actor.train()
+        if exploration:
+            noise = Variable(torch.from_numpy(self.random_process.sample()).float())
+            if use_cuda:
+                noise = noise.cuda()
+            action = action + noise
+        return action
+    
+    def reset(self):
+        self.random_process.reset()
+    
+class ParamNoisePolicy:
+
+    def __init__(self, actor, batch_size, memory):
+        self.actor = actor
+        self.param_noise = ParamNoise(batch_size, memory)
+        self.perturbed_model = self.param_noise.perturb_model(self.actor)
+
+    def select_action(self, state, exploration=True)
+        if use_cuda:
+            state = state.cuda()
+        if exploration:
+            self.perturbed_model.eval()
+            action = self.perturbed_model(state)
+            self.perturbed_model.train()
+            self.param.noise.update_sigma(self.actor, perturbed_model)
+        else:
+            self.actor.eval()
+            action = self.actor(state)
+            self.actor.train()
+        return action
+        
+    def reset(self):
+        self.param_noise.reset()
+        self.perturbed_model = self.param_noise.perturb_model(self.actor)
+        
+        
+        
