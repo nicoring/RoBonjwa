e7ac2df85aa47806608ae74f924bee3053fc55cd
Nico Ring <ringnico@web.de>
2018-03-15 00:55:04 +0000

    update run cluster script

--- run_cluster.sh
+++ run_cluster.sh
@@ -2,11 +2,13 @@
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --gres=gpu:1
+#SBATCH -c 4
 #SBATCH --mem=16000  # memory in Mb
 #SBATCH -o logs/out  # send stdout to sample_experiment_outfile
 #SBATCH -e logs/err  # send stderr to sample_experiment_errfile
 #SBATCH -t 8:00:00  # time requested in hour:minute:secon
 #SBATCH --signal=USR1
+#SBATCH --nodelist=landonia[12,17,21,23]
 
 #To be used before srun so that interactive sessions are run with gpu support
 export CUDA_HOME=/opt/cuda-8.0.44
@@ -31,6 +33,6 @@ export TMPDIR=/disk/scratch/${STUDENT_ID}/
 export TMP=/disk/scratch/${STUDENT_ID}/
 
 # Activate the relevant virtual environment:
-source /home/${STUDENT_ID}/miniconda3/bin/activate roboschool
+source /home/${STUDENT_ID}/miniconda3/bin/activate mujoco
 
 "$@"
